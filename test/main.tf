e925e6a4d814476ade88444c405e3f14c8e29f81
naveen <naveentharapatla@gmail.com>
2026-02-25 23:41:43 +0530

    change ami type

--- test/main.tf
+++ test/main.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {    
     bucket = "terraform-states-prod1"
     key    = "test-eks/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
 
     role_arn = "arn:aws:iam::907143134003:role/TerraformStateAccessRole"
   }

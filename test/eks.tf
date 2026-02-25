cd32b9675fab44df0edaddb3a7aa91c2c0abdd4f
naveen <naveentharapatla@gmail.com>
2026-02-25 23:44:14 +0530

    choose correct region

--- test/eks.tf
+++ test/eks.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "network" {
   config = {
     bucket = "terraform-states-prod1"
     key    = "test4/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
     role_arn = "arn:aws:iam::907143134003:role/TerraformStateAccessRole"
   }
 

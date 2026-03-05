54bde7e15ff132b52c04e41db22c8fb956c18a59
naveen <naveentharapatla@gmail.com>
2026-03-05 15:59:04 +0530

    rebuilt the indrastructure

--- test/auths.tf
+++ test/auths.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_config_map_v1" "aws_auth" {
+resource "kubernetes_config_map_v1_data" "aws_auth" {
   metadata {
     name      = "aws-auth"
     namespace = "kube-system"
@@ -32,6 +32,8 @@ resource "kubernetes_config_map_v1" "aws_auth" {
       }
     ])
   }
+
+  force = true
 
   depends_on = [
     aws_eks_node_group.managed_node_group
